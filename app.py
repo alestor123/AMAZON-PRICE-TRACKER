952e8acdf651af64914767097ddfa271103ac790
Alestor Aldous <alestoraldous@gmail.com>
2020-07-18 21:44:07 +0530

    sending request to fetch data

--- app.py
+++ app.py
@@ -6,3 +6,5 @@ import time
 headers = {
 "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36"
 }
+# send a request to fetch HTML of the page
+response = requests.get('https://www.amazon.in/Lenovo-L22e-20-Monitor-Display-inputs/dp/B07SGDWMCG/ref=sr_1_1?dchild=1&keywords=monitor+lenovo&qid=1595088224&s=electronics&sr=1-1', headers=headers)
