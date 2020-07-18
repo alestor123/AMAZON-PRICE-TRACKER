0e94b4f034650a7945c3adb5b43663ccdda9d425
Alestor Aldous <alestoraldous@gmail.com>
2020-07-18 21:43:27 +0530

    added browser data options

--- app.py
+++ app.py
@@ -2,3 +2,7 @@
 import requests
 from bs4 import BeautifulSoup
 import time
+# set the headers and user string
+headers = {
+"User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36"
+}
